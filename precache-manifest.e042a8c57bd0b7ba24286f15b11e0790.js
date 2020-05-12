1c5e1b9342f46cddcd27cd0d09321a3b9b69a689
alexander-braun <alexanderbraun@freenet.de>
2020-05-13 01:07:10 +0200

    Updates

---
+++ precache-manifest.e042a8c57bd0b7ba24286f15b11e0790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ad6b9916572c6dd7eb6085aa5122df",
+    "url": "/Youtube-Comment-Sentiment/index.html"
+  },
+  {
+    "revision": "b50947c33ff298f1287c",
+    "url": "/Youtube-Comment-Sentiment/static/css/main.7eee8f08.chunk.css"
+  },
+  {
+    "revision": "764bde0ff099c1e057d9",
+    "url": "/Youtube-Comment-Sentiment/static/js/2.b9e26f20.chunk.js"
+  },
+  {
+    "revision": "f3eb4ea049a8d0e8f09e402a9be089b0",
+    "url": "/Youtube-Comment-Sentiment/static/js/2.b9e26f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50947c33ff298f1287c",
+    "url": "/Youtube-Comment-Sentiment/static/js/main.fd5a2cb5.chunk.js"
+  },
+  {
+    "revision": "daf311dbe186f4eeb6fd",
+    "url": "/Youtube-Comment-Sentiment/static/js/runtime-main.cb30e180.js"
+  }
+]);
